1286bf655afc2cb1dab9aad8370c2c16e5b16977
Antonin Huaut <antonin.huaut@gmail.com>
2022-04-22 19:54:15 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import {computed, ref} from "vue";
+
+const types: string[] = ["betty", "blockfish", "brinely", "clayfish", "dasher", "flopper", "glitter", "kob", "snooper", "spotty", "stripey", "sunstreak"]
+const colors: string[] = ["blue", "brown", "cyan", "gray", "green", "light_blue", "light_gray", "lime", "magenta", "orange", "pink", "purple", "red", "white", "yellow"]
+
+let type = ref(types[1])
+let primaryColor = ref(colors[0])
+let secondaryColor = ref(colors[0])
+
+const imgSrc = computed(() => `/textures/${type.value}/${primaryColor.value}/${secondaryColor.value}.png`)
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div>
+      <div class="px-8 py-6 bg-white shadow-lg">
+        <div>
+          <label class="block" for="fish_type">Fish type</label>
+          <select
+              v-model="type"
+              id="fish_type"
+              class="w-full px-4 py-2 mt-2 border rounded-md"
+          >
+            <option :value="loopType" v-for="loopType in types">
+              {{ loopType }}
+            </option>
+          </select>
+        </div>
+        <div class="flex space-x-8 mt-4">
+          <div>
+            <label class="block" for="primary_color">Primary color</label>
+            <select
+                v-model="primaryColor"
+                id="primary_color"
+                class="w-full px-4 py-2 mt-2 border rounded-md"
+            >
+              <option :value="loopColor" v-for="loopColor in colors">
+                {{ loopColor }}
+              </option>
+            </select>
+          </div>
+
+          <div>
+            <label class="block" for="secondary_color">Secondary color</label>
+            <select
+                v-model="secondaryColor"
+                id="secondary_color"
+                class="w-full px-4 py-2 mt-2 border rounded-md"
+            >
+              <option :value="loopColor" v-for="loopColor in colors">
+                {{ loopColor }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div class="px-8 py-6">
+        <img class="h-96" :src="imgSrc" alt="Fish"/>
+      </div>
+    </div>
+  </div>
+</template>
